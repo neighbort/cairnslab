cdb6b1f067fc451994bec47db20946f14e1ef206
neighbort <inv.peace90144@gmail.com>
2025-06-07 22:38:00 +0900

    ordered indent and put startblocks out

---
+++ src/static/startblocks.js
@@ -0,0 +1,20 @@
+var startBlocks = {
+  blocks: {
+    languageVersion: 0,
+    blocks: [
+      {
+        type: 'text_print',
+        inputs: {
+          TEXT: {
+            block: {
+              type: 'text',
+              fields: {
+                TEXT: "Hello World!",
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+};
